4c353b653a01cc66a48a2fa2a79d66a379bf3500
joshmexe <joshmiranda.exe@gmail.com>
2021-03-11 15:30:00 -0500

    added some css test

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import flask
 from flask import Flask
 
-app = Flask(__name__)
+
+app = flask.Flask(__name__)
+
+def get_latest_packages():
+    return [
+        {'name' : 'flask', 'version' : '1.2.3'},
+        {'name': 'sqlalchmey', 'version': '2.2.0'},
+        {'name': 'passlib', 'version': '3.0.0'},
+    ]
+
 
 @app.route('/')
-def homepage():
-    return 'Hello, world!'
+def index():
+    test_packages = get_latest_packages()
+    return flask.render_template('index.html', packages = test_packages)
 
 
 if __name__ == '__main__':
